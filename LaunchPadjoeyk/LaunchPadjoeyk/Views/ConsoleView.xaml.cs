ee90c78d4cd04d55fcbc9c102e3c4de57f33d0f3
joeyk <jmkzpd@mst.edu>
2014-10-05 17:20:25 -0500

    added second datacontext

--- LaunchPadjoeyk/LaunchPadjoeyk/Views/ConsoleView.xaml.cs
+++ LaunchPadjoeyk/LaunchPadjoeyk/Views/ConsoleView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaunchPadjoeyk.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace LaunchPadjoeyk.Views
         public ConsoleView()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
