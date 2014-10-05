85516a85fa29c9f821217938bad32aa039abd7c0
joeyk <jmkzpd@mst.edu>
2014-10-05 17:26:05 -0500

    added mainwindowmodel to mainwindowviewmodel

--- LaunchPadjoeyk/LaunchPadjoeyk/ViewModels/MainWindowViewModel.cs
+++ LaunchPadjoeyk/LaunchPadjoeyk/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaunchPadjoeyk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace LaunchPadjoeyk.ViewModels
 {
     class MainWindowViewModel
     {
+        private MainWindowModel Model;
     }
 }
