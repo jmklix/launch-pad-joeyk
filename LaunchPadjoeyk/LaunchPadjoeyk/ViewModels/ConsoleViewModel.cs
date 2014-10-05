0f25f0a4dc0ee64d5c9811162d4c1eeb88cee74c
joeyk <jmkzpd@mst.edu>
2014-10-05 17:24:49 -0500

    added consolemodel to consoleviewmodel

--- LaunchPadjoeyk/LaunchPadjoeyk/ViewModels/ConsoleViewModel.cs
+++ LaunchPadjoeyk/LaunchPadjoeyk/ViewModels/ConsoleViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using LaunchPadjoeyk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LaunchPadjoeyk.ViewModels
 {
     class ConsoleViewModel
     {
+        private ConsoleModel Model;
     }
 }
